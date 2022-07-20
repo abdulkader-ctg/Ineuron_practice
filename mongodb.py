7c278e53ca640429b1e0ff631f8ca708c06e6c2e
abdulkader-ctg <scl.abdulkader@gmail.com>
2022-07-21 00:07:12 +0600

    this is my 1st code

--- mongodb.py
+++ mongodb.py
@@ -1,11 +1,11 @@
 import pymongo
 
-client = pymongo.MongoClient("mongodb+srv://sclabdulkader:Scomm2022@cluster0.efrrc.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb+srv://sclabdulkader:Scom2022@cluster0.efrrc.mongodb.net/?retryWrites=true&w=majority")
 db = client.test
 print(db)
 
 
-d = {
+d={
     "name":"sudhanshu",
     "email" : "sudhanshu@ineuron.ai",
     "surname" : "kumar"
